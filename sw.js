5c07d3a5211f41b048ede68eb73f96cf92105305
dimitryu <30570758+dimitryu@users.noreply.github.com>
2026-05-04 03:20:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ╔══════════════════════════════════════════╗
 // ║  Family Location — Service Worker v1.0.6 ║
 // ╚══════════════════════════════════════════╝
-const CACHE = 'family-location-v1.3.1';
+const CACHE = 'family-location-v1.4.0';
 const ASSETS = [
   './',
   './index.html',
